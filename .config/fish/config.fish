fe8fdef6144c6bb2ac8af0fd5e4f2766bcbe5d27
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-03-30 20:03:48 +0200

    Set fish_user_paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,3 +25,11 @@ abbr py python
 abbr fl 'floyd logs -t'
 abbr fs 'floyd status'
 abbr fr 'floyd run --tensorboard --env tensorflow-1.5'
+
+if not set -q -U fish_user_paths
+  set -U fish_user_paths "$HOME/.local/bin"
+  if test $_platform = "darwin"
+    set -U fish_user_paths $fish_user_paths "$HOME/Library/Python/2.7/bin"
+    set -U fish_user_paths $fish_user_paths "$HOME/Library/Python/3.6/bin"
+  end
+end
