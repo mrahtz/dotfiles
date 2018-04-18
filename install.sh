54fb6b2f92c6339e8b4d53d0154aa20f79cf44c3
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-18 23:06:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git clone --bare https://github.com/mrahtz/dotfiles ~/.dotfiles
-function config { git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@ }
+function config { git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@; }
 config config status.showUntrackedFiles no
 config checkout
