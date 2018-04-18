327353bf77231cf3a65b4c58133f84d9cc06dc99
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-18 22:50:53 +0200

    Update config.fish

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
-function config --description 'alias config=git --git-dir=/Users/matthew/.dotfiles --work-tree=/Users/matthew'
+function config
 	git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv;
 end
