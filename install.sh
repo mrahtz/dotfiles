a71c47d6807edd64c364600021a71c2e491092a9
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-30 00:24:10 +0200

    Create directories in ~/.confbackup and remove install.sh and README.md

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+set -o errexit
+
 git clone --bare https://github.com/mrahtz/dotfiles ~/.dotfiles
 function config { git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@; }
 config config status.showUntrackedFiles no
 checkout_output=$(config checkout 2>&1)
 if [[ $? != 0 ]]; then
     mkdir ~/.confbackup
-    egrep "\s+\." <<< "$checkout_output" | awk '{print $1}' | xargs -I{} mv "{}" ~/.confbackup/"{}"
+    egrep "\s+\." <<< "$checkout_output" | awk '{print $1}' | while read f; do
+    mkdir -p ~/.confbackup/"$(dirname $f)"
+        mv "$f" ~/.confbackup/"$f"
+    done
     echo "Existing configuration backed up to .confbackup."
     config checkout
 fi
+rm README.md install.sh
 config submodule update --init --recursive
