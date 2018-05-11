086bd410fec9c7fe25900f5faa387cf60daef0d9
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-05-11 12:37:03 +0200

    Fix properly

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,29 @@
+alias gwd='git diff --word-diff=color'
+alias gwdc='git diff --word-diff=color --cached'
+alias gpl='git pull'
+alias gps='git push'
+alias gc='git commit'
+alias gco='git checkout'
+alias gcm='git commit -m'
+alias gca='git commit --amend'
+alias ga='git add'
+alias gs='git status'
+alias gr='git rev-parse --short HEAD'
+alias gb='git branch'
+alias gcp='git cherry-pick'
+
+alias f='find . -iname'
+alias g=grep
+alias gv='grep -v'
+
+alias p8='ping 8.8.8.8'
+
+alias py=python
+
+alias fl='floyd logs -t'
+alias fs='floyd status'
+alias fr='floyd run --tensorboard --env tensorflow-1.5'
+
 if not set -q -U fish_user_paths
   set -U fish_user_paths "$HOME/.local/bin"
   if test $_platform = "darwin"
