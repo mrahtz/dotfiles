a40e5acc131621c495240d40086d2e791e81623a
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-12-13 13:12:51 -0800

    Auto import pickle

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,5 +1,6 @@
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2',
                                     'import numpy as np',
+                                    'import pickle',
                                     'from pylab import *']
 c.StoreMagics.autorestore = True
