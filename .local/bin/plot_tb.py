d26d16ae3a88add21a659f032ef4aae796b90170
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-07-03 02:50:50 -0700

    tb: use separate TMPDIR

--- .local/bin/plot_tb.py
+++ .local/bin/plot_tb.py
@@ -7,7 +7,7 @@ import os
 from math import ceil
 
 import tensorflow as tf
-from pylab import figure, plt, savefig, subplot, figlegend, tight_layout
+from pylab import plt, savefig, figlegend, tight_layout
 
 plt.rcParams.update({'figure.max_open_warning': 0})
 
@@ -42,7 +42,7 @@ def read_all_events(log_dirs):
         for event_file in find_files_matching_pattern('events.out.tfevents*', log_dir):
             log_dir_event_file_tuples.append((log_dir, event_file))
 
-    pool = multiprocessing.Pool(processes=32)
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
     tuple_log_dirs, tuple_event_files = list(zip(*log_dir_event_file_tuples))
     events_in_each_file = pool.map(read_events_file, tuple_event_files)
     for log_dir, events in zip(tuple_log_dirs, events_in_each_file):
@@ -73,52 +73,71 @@ def plot_values(ax, y_values, x_values, yname, xname, title_str, label, color, s
     return line
 
 
-def main():
+def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('log_dir', nargs='*')
     parser.add_argument('--n_cols', type=int, default=4)
     parser.add_argument('--smoothing', type=float, default=0.6)
     args = parser.parse_args()
-
     if not args.log_dir:
         raise Exception("Specify at least one log directory")
     for d in args.log_dir:
         if not os.path.exists(d):
-            raise Exception(f"Directory {d} does not exist")
+            raise Exception(f"Directory '{d}' does not exist")
+        if not os.path.isdir(d):
+            raise Exception(f"'{d}' is not a directory")
+    return args
+
+
+def scale_timestamps(timestamps):
+    t = max(timestamps)
+    if t < 60:
+        d = 1
+        units = 'seconds'
+    elif t < 3600:
+        d = 60
+        units = 'minutes'
+    else:
+        d = 3600
+        units = 'hours'
+    for n in range(len(timestamps)):
+        timestamps[n] /= d
+    return units
+
+
+def main():
+    args = get_args()
 
     print("Reading events...")
     events_by_log_dir = read_all_events(args.log_dir)
 
-    i = len(os.path.commonprefix(args.log_dir))
-    len_longest_suffix = len(os.path.commonprefix([s[::-1] for s in args.log_dir]))
-    if len_longest_suffix > 0:
-        j = -len_longest_suffix
-    else:
-        j = None
-
     print("Plotting...")
+
+    all_tags = sorted(set([key for events in events_by_log_dir.values() for key in events.keys()]))
+    n_graphs = len(all_tags)
+    if n_graphs < args.n_cols:
+        args.n_cols = n_graphs
+    w, h = (int(ceil(n_graphs / args.n_cols)), args.n_cols)
+    fig, axes = plt.subplots(w, h, figsize=(6 * h, 4 * w), squeeze=False)
+    axes = [ax for l in axes for ax in l]
+    axes_by_tag = {tag: axes[n] for n, tag in enumerate(all_tags)}
     lines, labels = [], []
-    plot_n = 1
-    axes = {}
-    n_graphs = len(set([key for events in events_by_log_dir.values() for key in events.keys()])) + 1
-    subplot_dims = (int(ceil(n_graphs / args.n_cols)), args.n_cols)
-    figure(figsize=(6 * subplot_dims[1], 4 * subplot_dims[0]))
     for log_dir_n, (log_dir, events) in enumerate(events_by_log_dir.items()):
         print(log_dir)
         color = f"C{log_dir_n % 10}"
-        label = log_dir[i:j]
-        for key, events in events.items():
-            if key not in axes:
-                axes[key] = subplot(*(subplot_dims + (plot_n,)))
-                plot_n += 1
-            ax = axes[key]
+        label = log_dir
+        for tag, events in events.items():
+            ax = axes_by_tag[tag]
             timestamps, values = list(zip(*events))
             relative_timestamps = [t - timestamps[0] for t in timestamps]
-            relative_timestamps_hours = [t / 3600 for t in relative_timestamps]
-            line = plot_values(ax, values, relative_timestamps_hours, 'Value', 'Time (hours)', key,
+            units = scale_timestamps(relative_timestamps)
+            line = plot_values(ax, values, relative_timestamps, 'Value', f'Time ({units})', tag,
                                label=label, color=color, smoothing=args.smoothing)
             lines.append(line)
             labels.append(label)
+    for n in range(len(all_tags), len(axes)):
+        axes[n].axis('off')
+    tight_layout()
 
     unique_lines = []
     unique_labels = []
@@ -126,19 +145,19 @@ def main():
         if label not in unique_labels:
             unique_labels.append(label)
             unique_lines.append(line)
-    figlegend(unique_lines, unique_labels, loc='upper center')
-    tight_layout(rect=[0, 0, 1, 0.90])
+    figlegend(unique_lines, unique_labels, bbox_to_anchor=(0.5, 1.0), loc='lower center')
 
     print("Saving figure...")
     # Removing trailing slashes
-    normed_log_dirs = [os.path.normpath(d) for d in args.log_dir]
-    if len(args.log_dir) == 1:
-        out_filename = os.path.basename(normed_log_dirs[0])
-    else:
-        longest_common_prefix = os.path.commonprefix(normed_log_dirs)
-        longest_common_suffix = os.path.commonprefix([d[::-1] for d in normed_log_dirs])[::-1]
-        out_filename = longest_common_prefix + '*' + longest_common_suffix + '.png'
-    savefig(out_filename)
+    out_filename = 'foo.png'
+    # normed_log_dirs = [os.path.normpath(d) for d in args.log_dir]
+    # if len(args.log_dir) == 1:
+    #     out_filename = os.path.basename(normed_log_dirs[0])
+    # else:
+    #     longest_common_prefix = os.path.commonprefix(normed_log_dirs)
+    #     longest_common_suffix = os.path.commonprefix([d[::-1] for d in normed_log_dirs])[::-1]
+    #     out_filename = longest_common_prefix + '*' + longest_common_suffix + '.png'
+    savefig(out_filename, bbox_inches='tight')
 
 
 if __name__ == '__main__':
