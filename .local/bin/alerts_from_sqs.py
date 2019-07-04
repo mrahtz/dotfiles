5de1f8b5b50754377118c135865cc12b1742fb5a
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-07-04 17:03:02 +0200

    alerts_from_sqs: misc fixes

--- .local/bin/alerts_from_sqs.py
+++ .local/bin/alerts_from_sqs.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Start on login with
-    $ launchctl load .local/bin/alerts_from_sqs.plist
+Poll an AWS Simple Queue Services queue for messages,
+and when one is received, use AppleScript to pop up an alert
+with the contents of the message.
 """
 
 import subprocess
@@ -10,21 +11,27 @@ import time
 
 import boto3
 
+QUEUE_URL = 'https://sqs.us-west-2.amazonaws.com/469197347721/alerts.fifo'
+
 
 def alert(text):
-    script = f'display notification "{text}" with title "Job monitor"'
+    script = f'display notification "{text}" with title "Alert"'
     script += ' sound name "Frog"'
     cmd = ['osascript', '-e', script]
     subprocess.call(cmd)
 
 
-sqs = boto3.resource('sqs')
-queue = sqs.get_queue_by_name(QueueName='alerts.fifo')
+session = boto3.Session(profile_name='sqs_alerts')
+sqs = session.client('sqs')
 while True:
+    print(time.time())
     try:
-        for message in queue.receive_messages(WaitTimeSeconds=1):
-            alert(message.body)
-            message.delete()
+        response = sqs.receive_message(QueueUrl=QUEUE_URL, WaitTimeSeconds=1)
+        if not 'Messages' in response:
+            continue
+        for message in response['Messages']:
+            alert(message['Body'])
+            sqs.delete_message(QueueUrl=QUEUE_URL, ReceiptHandle=message['ReceiptHandle'])
     except Exception as e:
-        print(e)
+        print('Exception:', e)
         time.sleep(1)
