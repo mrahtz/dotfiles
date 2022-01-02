050fd1b6c664b72c62efd7f42d2d945e8bbb3b11
Matthew Rahtz <matthew.rahtz@gmail.com>
2022-01-02 23:18:05 +0000

    Fix IPython config

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,4 +1,8 @@
-c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['%autoreload 2',
-                                    'import numpy as np',
-                                    'from pylab import *']
+c.InteractiveShellApp.extensions = [
+    'autoreload',
+]
+c.InteractiveShellApp.exec_lines = [
+    '%autoreload 2',
+    'import numpy as np',
+    'import matplotlib.pyplot as plt',
+]
